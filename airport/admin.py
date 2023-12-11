8996d1b458b6bdc5fa8e026864b1f5e45c21b08b
Roman Burluka <103953904+romkapomka12@users.noreply.github.com>
2023-12-11 16:38:32 +0200

    The Order, Flight, Airplane, Crew, Airport, AirplaneType, Route and Ticket models were registered for display in the administrative interface.

--- airport/admin.py
+++ airport/admin.py
@@ -1,17 +1,34 @@
 from django.contrib import admin
-
+from .forms import FlightForm
 from airport.models import (Flight,
                             Crew,
                             Airport,
                             AirplaneType,
                             Route,
                             Order,
-                            Ticket)
+                            Ticket,
+                            Airplane)
 
-admin.site.register(Flight)
+
+class FlightAdmin(admin.ModelAdmin):
+    form = FlightForm
+
+
+class TicketInLine(admin.TabularInline):
+    model = Ticket
+    extra = 1
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    inlines = (TicketInLine,)
+
+
+admin.site.register(Flight, FlightAdmin)
+admin.site.register(Airplane)
 admin.site.register(Crew)
 admin.site.register(Airport)
 admin.site.register(AirplaneType)
 admin.site.register(Route)
-admin.site.register(Order)
+
 admin.site.register(Ticket)
