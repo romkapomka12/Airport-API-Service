d34715a9d0b3788c94bb0b0bf3514ef4274d9694
Roman Burluka <103953904+romkapomka12@users.noreply.github.com>
2023-12-12 13:18:44 +0200

    Replaced the username field with an email field in the user model. Added AuthTokenSerializer so we can get token with email and password

--- user/serializers.py
+++ user/serializers.py
@@ -1,4 +1,5 @@
-from django.contrib.auth import get_user_model
+from django.contrib.auth import get_user_model, authenticate
+from django.utils.translation import gettext as _
 from rest_framework import serializers
 
 
@@ -10,9 +11,15 @@ class UserSerializer(serializers.ModelSerializer):
         extra_kwargs = {"password": {"write_only": True, "min_length": 6}}
 
     def create(self, validated_data):
+
+        """Create a new user with encrypted password and return it"""
+
         return get_user_model().objects.create_user(**validated_data)
 
     def update(self, instance, validated_data):
+
+        """Update a user, set the password correctly and return it"""
+
         password = validated_data.pop("password", None)
         user = super().update(instance, validated_data)
 
@@ -21,3 +28,34 @@ class UserSerializer(serializers.ModelSerializer):
             user.save()
 
         return user
+
+
+class AuthTokenSerializer(serializers.Serializer):
+    email = serializers.EmailField(label="Email")
+    password = serializers.CharField(
+        label="Password",
+        style={"input_type": "password"},
+        trim_whitespace=False
+    )
+
+    def validate(self, attrs):
+        email = attrs.get("email")
+        password = attrs.get("password")
+
+        if email and password:
+            user = authenticate(
+                request=self.context.get("request"),
+                email=email,
+                password=password
+            )
+            if not user:
+                msg = _("Unable to log in with provided credentials.")
+                raise serializers.ValidationError(msg, code="authorization")
+            if not user.is_active:
+                msg = _("User account is disabled.")
+                raise serializers.ValidationError(msg, code="authorization")
+        else:
+            msg = _('Must include "email" and "password".')
+            raise serializers.ValidationError(msg, code="authorization")
+
+        return {"user": user}
