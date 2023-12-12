577762253df571c64b2764addf8b71cecd509570
Roman Burluka <103953904+romkapomka12@users.noreply.github.com>
2023-12-12 13:15:35 +0200

    This means that the settings for saving and displaying media files are configured

--- airport_service/urls.py
+++ airport_service/urls.py
@@ -13,7 +13,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
@@ -21,7 +22,8 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/airport_service/", include("airport.urls",  namespace="airport")),
-    # path("api/user/", include("user.urls")),
+    path("api/user/", include("user.urls",  namespace="user")),
     path("__debug__/", include("debug_toolbar.urls")),
 
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
