c2601aec9a7d909523f6157e92ee7cd5e7fb5800
Roman Burluka <103953904+romkapomka12@users.noreply.github.com>
2023-12-20 12:27:11 +0200

    Added permissions for administrator and authenticated users

--- airport/permissions.py
+++ airport/permissions.py
@@ -12,8 +12,5 @@ class IsAdminOrIfAuthenticatedReadOnly(BasePermission):
             request.method in SAFE_METHODS
             and request.user
             and request.user.is_authenticated
-            or (
-                    request.user
-                    and request.user.is_staff
-            )
+            or (request.user and request.user.is_staff)
         )
