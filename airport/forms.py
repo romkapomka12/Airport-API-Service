91f484db3e91c7cad64c4dbf48b7936b255fa4f0
Roman Burluka <103953904+romkapomka12@users.noreply.github.com>
2023-12-20 12:16:49 +0200

    Created a Django RouteForm. This form is used to validate user input before saving it to the Route model.

--- airport/forms.py
+++ airport/forms.py
@@ -2,7 +2,7 @@ from django import forms
 from django.core.exceptions import ValidationError
 from django.utils.translation import gettext_lazy as _
 
-from airport.models import Flight
+from airport.models import Flight, Route
 
 
 def validate_crew(value):
@@ -11,6 +11,11 @@ def validate_crew(value):
             _("%(value)s is not a valid crew size"),
             params={"value": value},
         )
+
+
+def validate_route(self):
+    if self.source == self.destination:
+        raise ValidationError("The city of departure and arrival cannot be the same")
 
 
 class FlightForm(forms.ModelForm):
@@ -22,3 +27,13 @@ class FlightForm(forms.ModelForm):
         crew = self.cleaned_data.get("crew")
         validate_crew(crew.count())
         return crew
+
+
+class RouteForm(forms.ModelForm):
+    class Meta:
+        model = Route
+        fields = ["source", "destination", "distance"]
+
+    def clean_route(self):
+        route = self.cleaned_data.get("route")
+        return route
