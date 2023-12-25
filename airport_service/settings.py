107bb088bdc2498d7741ba10ac7d7f55bb00aff7
Roman Burluka <103953904+romkapomka12@users.noreply.github.com>
2023-12-25 12:12:21 +0200

    adding some instructions on how to run the project locally without docker.

--- airport_service/settings.py
+++ airport_service/settings.py
@@ -81,10 +81,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = "airport_service.wsgi.application"
 
-
-# Database
-# https://docs.djangoproject.com/en/4.1/ref/settings/#databases
-
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
